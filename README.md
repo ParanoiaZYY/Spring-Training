f34edd9cdca867c959a91fab11b08480563d314f
Paranoia <wqd_1994@163.com>
2019-09-17 19:22:36 +0800

    调整工程目录结构

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Spring-Trarning
 Spring Trarning Demos
+
+## Spring 
+spring context
+spring aop
+spring beans
+spring web mvc
+
+## Spring Boot
+
+## Spring Cloud 
+
+## Spring Cloud Alibaba
+
---
+++ spring-boot/spring-boot-lombok/src/main/java/site/paranoia/TestController.java
@@ -0,0 +1,17 @@
+package site.paranoia;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Paranoia on 2019/9/10 人间不值得
+ **/
+@RestController(value = "/test")
+public class TestController {
+
+    @PostMapping
+    public void test(@RequestBody Test test) {
+
+    }
+}
