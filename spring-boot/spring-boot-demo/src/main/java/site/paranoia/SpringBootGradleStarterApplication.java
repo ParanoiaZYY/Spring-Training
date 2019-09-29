6be196ea1ca471c088b6e1929a50fc5586af3978
Paranoia <wqd_1994@163.com>
2019-09-29 09:34:51 +0800

    spring-boot-demo add liquibase

--- spring-boot/spring-boot-demo/src/main/java/site/paranoia/SpringBootGradleStarterApplication.java
+++ spring-boot/spring-boot-demo/src/main/java/site/paranoia/SpringBootGradleStarterApplication.java
@@ -2,11 +2,14 @@ package site.paranoia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @EnableWebMvc
 @SpringBootApplication
+@EnableConfigurationProperties({LiquibaseProperties.class})
 @MapperScan("site.paranoia.mapper")
 public class SpringBootGradleStarterApplication {
 
---
+++ spring-boot/spring-boot-demo/src/main/java/site/paranoia/config/LiquibaseConfiguration.java
@@ -0,0 +1,69 @@
+package site.paranoia.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class LiquibaseConfiguration {
+
+    private final Logger log = LoggerFactory.getLogger(LiquibaseConfiguration.class);
+
+    private final Environment env;
+
+    public LiquibaseConfiguration(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public SpringLiquibase liquibaseForBusiness(
+                                     DataSource dataSource, LiquibaseProperties liquibaseProperties) {
+
+        // Use liquibase.integration.spring.SpringLiquibase if you don't want Liquibase to start asynchronously
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:config/liquibase/business/master.xml");
+        liquibase.setContexts(liquibaseProperties.getContexts());
+        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+        liquibase.setChangeLogParameters(liquibaseProperties.getParameters());
+        liquibase.setDatabaseChangeLogTable("changelog_table_business");
+        liquibase.setDatabaseChangeLogLockTable("changelog_lock_table_business");
+        if (env.acceptsProfiles("no-liquibase")) {
+            liquibase.setShouldRun(false);
+        } else {
+            liquibase.setShouldRun(liquibaseProperties.isEnabled());
+            log.debug("Configuring Liquibase");
+        }
+        return liquibase;
+    }
+
+    @Bean
+    public SpringLiquibase liquibaseForFinancial(
+                                     DataSource dataSource, LiquibaseProperties liquibaseProperties) {
+
+        // Use liquibase.integration.spring.SpringLiquibase if you don't want Liquibase to start asynchronously
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+        liquibase.setChangeLog("classpath:config/liquibase/financial/master.xml");
+        liquibase.setContexts(liquibaseProperties.getContexts());
+        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+        liquibase.setChangeLogParameters(liquibaseProperties.getParameters());
+        liquibase.setDatabaseChangeLogTable("changelog_table_fin");
+        liquibase.setDatabaseChangeLogLockTable("changelog_lock_table_fin");
+        if (env.acceptsProfiles("no-liquibase")) {
+            liquibase.setShouldRun(false);
+        } else {
+            liquibase.setShouldRun(liquibaseProperties.isEnabled());
+            log.debug("Configuring Liquibase");
+        }
+        return liquibase;
+    }
+}
