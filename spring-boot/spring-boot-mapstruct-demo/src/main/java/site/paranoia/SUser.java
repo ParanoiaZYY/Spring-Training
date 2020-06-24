366348f6b860b2a84d0043946a896ccdf9ca43a2
wqd1994 <wqd_1994@163.com>
2020-06-24 16:38:49 +0800

    add spring-boot-mapstruct-demo

---
+++ spring-boot/spring-boot-mapstruct-demo/src/main/java/site/paranoia/SUser.java
@@ -0,0 +1,12 @@
+package site.paranoia;
+
+import lombok.Data;
+
+/**
+ * Created by Paranoia on 2020/6/24 人间不值得
+ **/
+@Data
+public class SUser {
+    private String userName;
+    private String realName;
+}
---
+++ spring-boot/spring-boot-mapstruct-demo/src/main/java/site/paranoia/SUserMapStruct.java
@@ -0,0 +1,12 @@
+package site.paranoia;
+
+import org.mapstruct.Mapper;
+
+/**
+ * Created by Paranoia on 2020/6/24 人间不值得
+ **/
+@Mapper
+public interface SUserMapStruct {
+    SUserDTO toDTO(SUser sUser);
+    SUser toEntity(SUserDTO sUserDTO);
+}
