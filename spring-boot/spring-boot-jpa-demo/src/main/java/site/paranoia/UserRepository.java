b24ec1c80a46c3593cc456ba4d8ad583f2d6005e
wqd1994 <wqd_1994@q63.com>
2020-09-29 14:00:19 +0800

    删除无用文件

--- spring-boot/spring-boot-jpa-demo/src/main/java/site/paranoia/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package site.paranoia;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import site.paranoia.domain.SUser;
-
-@Repository
-public interface UserRepository extends JpaRepository<SUser, Integer> {
-}
