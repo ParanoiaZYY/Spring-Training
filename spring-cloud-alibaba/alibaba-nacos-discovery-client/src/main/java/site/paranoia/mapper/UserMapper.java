1000d35e059927139f721d467dfba495e0a74ba3
Paranoia <wqd_1994@163.com>
2020-10-08 16:11:59 +0800

    delete spring cloud alibaba demo

--- spring-cloud-alibaba/alibaba-nacos-discovery-client/src/main/java/site/paranoia/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package site.paranoia.mapper;
-
-import site.paranoia.domain.User;
-import site.paranoia.utils.MyMapper;
-
-public interface UserMapper extends MyMapper<User> {
-
-}
--- spring-cloud-alibaba/dubbo-api/src/main/java/site/paranoia/api/service/HelloService.java
+++
@@ -1,7 +0,0 @@
-package site.paranoia.api.service;
-
-import site.paranoia.api.domain.UserRequest;
-
-public interface HelloService {
-    String sayHello(UserRequest userRequest);
-}
