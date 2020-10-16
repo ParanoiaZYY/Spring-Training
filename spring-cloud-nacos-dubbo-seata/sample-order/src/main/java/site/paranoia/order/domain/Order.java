65ba9bc6e9c5809a26b53c87b19563c000c6561e
wqd0214 <wqd_1994@163.com>
2020-10-16 14:32:10 +0800

    字编译seata处理SqlServer分布式事务问题

--- spring-cloud-nacos-dubbo-seata/sample-order/src/main/java/site/paranoia/order/domain/Order.java
+++ spring-cloud-nacos-dubbo-seata/sample-order/src/main/java/site/paranoia/order/domain/Order.java
@@ -9,7 +9,7 @@ import lombok.Data;
 @TableName("t_order")
 public class Order {
 
-    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
     private String orderNo;
     private String userId;
